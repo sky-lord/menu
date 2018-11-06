dfd771905ab4ce45184e5a633301bacf133281db
sky-lord <skylarleeson12@gmail.com>
2018-11-05 18:16:26 -0600

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,53 @@
-$("#recipeBtn").click(function(){
 
-  $('#recipeContent').html("");
+var pageCount;
+var apiString;
 
-var url = "https://api.edamam.com/search?app_id=9867db95&app_key=efd167d9a6093d14feffeb3b381b93ed&q=" + $('#recipeInput').val();
+$("#searchBtn").click(function(){
 
-  if($("#health1").is(':checked')){
-    url += '&health=vegetarian';
-  }
+  $('#gameContent').html('');
+  
+  //Get Movies
+  var mURL = 'https://api.themoviedb.org/3/search/movie?api_key=aba442bf0028b2c7957a6f762cf916c1&language=en-US&query=' + $('#gameInput').val() + '&page=1&include_adult=false';
 
-  if($("#health2").is(':checked')){
-    url += '&health=peanut-free';
-  }
+  var genreC = $("#genreCats option:selected").val();
+  if( genreC == "horror"){
+    
+  } else if( genreC == "horror"){
 
-    if($("#health3").is(':checked')){
-    url += '&diet=balanced';
-  }
+  } 
 
-    if($("#health4").is(':checked')){
-    url += '&calories=100-500';
-  }
 
-console.log(url);
-
-  $.getJSON(url, function(result){
-
-  }).done(function(data){
-    if( data.count != 0 ){
-    console.log(status);
-
-    data.hits.forEach(function(r,i){
-      var recipeItems = "";
-      data.hits[i].recipe.ingredients.forEach(function(recipeItem){
-        recipeItems += '<li>' + recipeItem.text + '</li>';
-      });
-      $('#recipeContent').append('<div class="recipeItem" data-menu="closed"><div class="recipeTitle">' + data.hits[i].recipe.label + '</div><img src="' + data.hits[i].recipe.image + '" class="recipeImage"><div class="yield">Serves ' + data.hits[i].recipe.yield + '</div><ul>' + recipeItems + '</ul></div>');
-      $(".recipeItem").click(function(){
-        $(this).css("height","auto");
-      });
+  $.getJSON(mURL, function(data){
+    data.results.forEach(function(movie){
+      $('#gameContent').append('<div class="movies"><img src="http://image.tmdb.org/t/p/w185/' + movie.poster_path + '" /><div class="titles">' + movie.original_title + '</div></div>');
     });
-  } else {
-    $('#recipeContent').html('<h4>error</h4>');
-    $('#recipeContent').append('<h1>Sorry, there was a problem finding a match.</h1>');
-  }
-  }).fail(function(jqxhr, textStatus, error){
-    $('#recipeContent').html('<h4>' + textStatus + '</h4>');
-    $('#recipeContent').append('<h1>Sorry, there was a problem finding a match.</h1>');
+    
+    console.log(data);
+  });
+
+  //Get Games
+  $.ajax({
+    url: 'https://api-endpoint.igdb.com/games/?search=' + $('#gameInput').val(),
+    //url: 'https://api-endpoint.igdb.com/games/41608?fields=*',
+    headers: { 
+      'user-key': '94e21d82c72f27c6295c8fa40523f83d',
+      'accept' : 'application/json' 
+    },
+    success: function(data){
+      data.forEach(function(game){
+        //$('#gameContent').append('<div class="game">' + game.id + '</div>');
+        $.ajax({
+          url: 'https://api-endpoint.igdb.com/games/' + game.id + '?fields=*',
+          headers: { 
+            'user-key': '94e21d82c72f27c6295c8fa40523f83d',
+            'accept' : 'application/json' 
+          },
+          success: function(gameData){
+            $('#gameContent').append('<div class="game"><img src="http:' + gameData[0].cover.url + '" /><a href="' + gameData[0].url + '"><div class="titles">' + gameData[0].name + '</a></div></div>');
+          }
+        });
+      })
+      console.log(data);
+    }
   });
 });
