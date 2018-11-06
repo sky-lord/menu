0941fdd4732eba057877f8a81244efa0ec47c1e2
sky-lord <skylarleeson12@gmail.com>
2018-11-05 18:14:22 -0600

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,33 @@
 $("#recipeBtn").click(function(){
-  
-  $("#health1").click(function(){
-  var url = "https://api.edamam.com/search?app_id=9867db95&app_key=efd167d9a6093d14feffeb3b381b93ed&health=vegetarian&q=" + $('#recipeInput').val();
-  });
 
- var url = "https://api.edamam.com/search?app_id=9867db95&app_key=efd167d9a6093d14feffeb3b381b93ed&q=" + $('#recipeInput').val();
+  $('#recipeContent').html("");
 
-  $.getJSON(url, function(data){
+var url = "https://api.edamam.com/search?app_id=9867db95&app_key=efd167d9a6093d14feffeb3b381b93ed&q=" + $('#recipeInput').val();
+
+  if($("#health1").is(':checked')){
+    url += '&health=vegetarian';
+  }
+
+  if($("#health2").is(':checked')){
+    url += '&health=peanut-free';
+  }
+
+    if($("#health3").is(':checked')){
+    url += '&diet=balanced';
+  }
+
+    if($("#health4").is(':checked')){
+    url += '&calories=100-500';
+  }
+
+console.log(url);
+
+  $.getJSON(url, function(result){
+
+  }).done(function(data){
+    if( data.count != 0 ){
+    console.log(status);
+
     data.hits.forEach(function(r,i){
       var recipeItems = "";
       data.hits[i].recipe.ingredients.forEach(function(recipeItem){
@@ -14,8 +35,15 @@ $("#recipeBtn").click(function(){
       });
       $('#recipeContent').append('<div class="recipeItem" data-menu="closed"><div class="recipeTitle">' + data.hits[i].recipe.label + '</div><img src="' + data.hits[i].recipe.image + '" class="recipeImage"><div class="yield">Serves ' + data.hits[i].recipe.yield + '</div><ul>' + recipeItems + '</ul></div>');
       $(".recipeItem").click(function(){
-          $(this).css("height","auto");
-        });
+        $(this).css("height","auto");
+      });
     });
+  } else {
+    $('#recipeContent').html('<h4>error</h4>');
+    $('#recipeContent').append('<h1>Sorry, there was a problem finding a match.</h1>');
+  }
+  }).fail(function(jqxhr, textStatus, error){
+    $('#recipeContent').html('<h4>' + textStatus + '</h4>');
+    $('#recipeContent').append('<h1>Sorry, there was a problem finding a match.</h1>');
   });
 });
